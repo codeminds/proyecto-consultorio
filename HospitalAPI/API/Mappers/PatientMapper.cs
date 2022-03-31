53f9203ce5a1cc73545d0a9445a63398e7fd5015
Victor Herrera <dsvictorh@gmail.com>
2022-03-30 22:30:12 -0600

    HospitalAPI: arreglar mapper de filtros de paciente.

--- HospitalAPI/API/Mappers/PatientMapper.cs
+++ HospitalAPI/API/Mappers/PatientMapper.cs
@@ -11,9 +11,9 @@ namespace API.Mappers
             CreateMap<Patient, GetPatientDTO>();
             CreateMap<CreateUpdatePatientDTO, Patient>();
             CreateMap<FilterAppointmentDTO, FilterPatientDTO>()
-                .ForMember(d => d.DocumentId, opt => opt.MapFrom(a => a.DoctorDocumentId))
-                .ForMember(d => d.FirstName, opt => opt.MapFrom(a => a.DoctorFirstName))
-                .ForMember(d => d.LastName, opt => opt.MapFrom(a => a.DoctorLastName))
+                .ForMember(d => d.DocumentId, opt => opt.MapFrom(a => a.PatientDocumentId))
+                .ForMember(d => d.FirstName, opt => opt.MapFrom(a => a.PatientFirstName))
+                .ForMember(d => d.LastName, opt => opt.MapFrom(a => a.PatientLastName))
                 .ForMember(d => d.BirthDateFrom, opt => opt.MapFrom(a => a.PatientBirthDateFrom))
                 .ForMember(d => d.BirthDateTo, opt => opt.MapFrom(a => a.PatientBirthDateTo))
                 .ForMember(d => d.Gender, opt => opt.MapFrom(a => a.PatientGender));
