f7756d827cec2af54790f8fe2697577e6d0413d9
Victor Herrera <vherrera.jobs@gmail.com>
2023-11-16 09:42:40 -0600

    HospitalWeb: arreglos menores.

--- HospitalWebLocal/assets/js/services/date.js
+++ HospitalWebLocal/assets/js/services/date.js
@@ -1,17 +1,17 @@
 export class DateService {
    /* Función para obtener una versión amigable para el UI de la fecha a partir de un objeto fecha */
    static toDisplayLocaleString(date, locale) {
-      if (date == null || !(date instanceof Date)) {
+      if (date == null || !(date instanceof Date) || isNaN(date)) {
          return '';
       }
 
-      return date.toLocaleString(locale, { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: true }).replace(/\.\xA0*/g, '').toUpperCase();
+      return date.toLocaleString(locale, { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: true }).replace(/(\.\xA0*|,)/g, '').toUpperCase();
    }
 
    /* Función para obtener un "string date", el tipo de texto de fecha con el formato requerido 
    para los input datetime-local, a partir de un objeto fecha */
    static toInputDateString(date) {
-      if (date == null || !(date instanceof Date)) {
+      if (date == null || !(date instanceof Date) || isNaN(date)) {
          return '';
       }
 
